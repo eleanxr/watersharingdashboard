a9ce9990cde55166b20de342efb6872c709d31bd
Will Dicharry <wdicharry@tnc.org>
2015-08-20 09:28:09 -0600

    Get environment variables from Apache in WSGI.

--- DjangoWebProject/settings/production.py
+++ DjangoWebProject/settings/production.py
@@ -8,12 +8,12 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME', "dbname"),
-        'USER': os.environ.get('DATABASE_USER', "dbuser"),
-        'PASSWORD': os.environ.get("DATABASE_PASSWORD", ""),
-        'HOST': os.environ.get("DATABASE_HOST", "dbhost"),
+        'NAME': os.environ.get('WATERSHARING_DATABASE_NAME', "dbname"),
+        'USER': os.environ.get('WATERSHARING_DATABASE_USER', "dbuser"),
+        'PASSWORD': os.environ.get("WATERSHARING_DATABASE_PASSWORD", ""),
+        'HOST': os.environ.get("WATERSHARING_DATABASE_HOST", "dbhost"),
         'PORT': '',
     }
 }
 
-SECRET_KEY = os.environ.get("SECRET_KEY", None)
+SECRET_KEY = os.environ.get("WATERSHARING_SECRET_KEY", None)
--- DjangoWebProject/wsgi.py
+++ DjangoWebProject/wsgi.py
@@ -15,14 +15,20 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoWebProject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoWebProject.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+_application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+def application(environ, start_response):
+    for key in environ:
+        if key.startswith("WATERSHARING_"):
+            os.environ[key] = environ[key]
+    return _application(environ, start_response)
