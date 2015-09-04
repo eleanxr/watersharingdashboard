38c3c74fc4ddff7cf1eca77d22cda8d3c264eb06
Will Dicharry <wdicharry@tnc.org>
2015-09-04 13:20:15 -0600

    Add please wait notifier while data loads.

--- flowviz/static/flowviz/scenario.js
+++ flowviz/static/flowviz/scenario.js
@@ -1,18 +1,41 @@
-var Scenario = (function (window, undefined) {
+(function (exports) {
 
-    function initialize() {
-        $('.img-wrapper > img').on('load', function () {
-            imgLoaded(this)
-        })
+    var imgCounter = 5;
+
+    function notifyComplete()  {
+        imgCounter--;
+        if (imgCounter <= 0) {
+            $("#pleaseWaitDialog").modal("hide");
+        }
     }
 
-    function imgLoaded(img) {
-        var $img = $(img);
-        $img.parent().addClass('loaded');
+    function initialize(scenario, imgUrls) {
+        $("#pleaseWaitDialog").modal();
+        downloadImage(imgUrls.average, "img-average")
+
+        var rasterTotalUrl = imgUrls.total + "/?cmap=spectral_r&title=" +
+            scenario.attribute_name + "+(" + scenario.attribute_units_abbr +
+            ")&logscale=True";
+        downloadImage(rasterTotalUrl, "img-total");
+
+        var rasterGapUrl = imgUrls.gap + "/?cmap=bwr_r&title=" +
+            scenario.attribute_name + "+gap+(" + scenario.attribute_units_abbr +
+            ")&zero=True";
+        downloadImage(rasterGapUrl, "img-gap");
+
+        downloadImage(imgUrls.stats, "img-stats");
+        downloadImage(imgUrls.pct, "img-pct");
+    }
+    exports.initialize = initialize;
+
+    function downloadImage(imgUrl, imgId) {
+        var image = document.getElementById(imgId);
+        var downloadingImage = new Image();
+        downloadingImage.onload = function () {
+            image.src = this.src;
+            notifyComplete();
+        };
+        downloadingImage.src = imgUrl;
     }
 
-    return {
-        initialize: initialize
-    }
-
-})(window)
+})(this.Scenario = {})
