08bf2dcb4e55de07dc547b0268b747bb24e182d9
Will Dicharry <wdicharry@tnc.org>
2015-09-17 13:54:20 -0600

    Cache water data for faster loads.

--- flowviz/cache_data.py
+++ flowviz/cache_data.py
@@ -1,10 +1,12 @@
 from waterkit import rasterflow
+import functools32
 
-
+@functools32.lru_cache(maxsize=32)
 def read_usgs_data(identifier, start_date, end_date, target, multiplier):
     return rasterflow.read_usgs_data(identifier, start_date, end_date, target,
         multiplier=multiplier)
 
+@functools32.lru_cache(maxsize=32)
 def read_excel_data(data_file, date_column_name, attribute_column_name,
     sheet_name, target_column_name, multiplier):
     return rasterflow.read_excel_data(
