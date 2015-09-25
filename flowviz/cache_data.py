378ca8e3a8941b2cb70832cac6da3974d07de953
Will Dicharry <wdicharry@tnc.org>
2015-09-25 14:22:27 -0600

    Add docstrings.

--- flowviz/cache_data.py
+++ flowviz/cache_data.py
@@ -17,6 +17,14 @@ def synchronized(f):
 
 @synchronized
 def read_usgs_data(identifier, start_date, end_date, target, multiplier):
+    """Read from a USGS data source.
+
+    This function is synchronized because requests for the same data are often
+    made in close proximity to one another, for instance when loading multiple
+    images on the same page. In such cases all threads waiting for data that
+    may be cached in the not too distant future is cheaper than all threads
+    doing the same work.
+    """
     data = read_usgs_data_impl(identifier, start_date, end_date, target, multiplier)
     logger.info("USGS data cache: %s", read_usgs_data_impl.cache_info())
     return data
@@ -29,6 +37,14 @@ def read_usgs_data_impl(identifier, start_date, end_date, target, multiplier):
 @synchronized
 def read_excel_data(data_file, date_column_name, attribute_column_name,
     sheet_name, target_column_name, multiplier):
+    """Read from an Excel data source.
+
+    This function is synchronized because requests for the same data are often
+    made in close proximity to one another, for instance when loading multiple
+    images on the same page. In such cases all threads waiting for data that
+    may be cached in the not too distant future is cheaper than all threads
+    doing the same work.
+    """
     data = read_excel_data_impl(data_file, date_column_name, attribute_column_name,
         sheet_name, target_column_name, multiplier)
     logger.info("Excel data cache: %s", read_excel_data_impl.cache_info())
