678c07e1d5a1efe14f5b30b4a12a3f9e9b22faa4
Will Dicharry <wdicharry@tnc.org>
2016-02-03 16:14:56 -0700

    Add an about page attributing NCEAS and SNAP.

--- flowviz/urls.py
+++ flowviz/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     url(r'projects/(?P<project_id>[0-9]+)/stats_annual_pct_csv/$', views.project_deficit_stats_annual_pct_csv, name='project_deficit_stats_annual_pct_csv'),
     url(r'projects/(?P<project_id>[0-9]+)/lowflow_pct_csv/$', views.project_low_flow_csv, name='project_low_flow_csv'),
     url(r'projects/(?P<project_id>[0-9]+)/lowflow_pct_plot/$', views.project_low_flow_plot, name='project_low_flow_plot'),
+    
+    #
+    # Scenario views
+    #
 
     url(r'scenario/(?P<scenario_id>[0-9]+)/$', views.scenario, name='scenario'),
 
