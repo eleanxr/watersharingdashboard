d9e1de0f5f110dbde3c783df58a63225bda53ea9
Will Dicharry <wdicharry@tnc.org>
2016-03-29 08:56:49 -0600

    Add missing import in admin.

--- scenarios/admin.py
+++ scenarios/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from models import GageLocation, CyclicTarget, CyclicTargetElement
 from models import Scenario
+from django.http import HttpResponseRedirect
 
 admin.site.register(GageLocation)
 
