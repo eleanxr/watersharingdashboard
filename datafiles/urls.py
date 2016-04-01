916d02f50d7b8787de22d9f7a94eb61527a2cd66
Will Dicharry <wdicharry@tnc.org>
2016-04-01 14:05:48 -0600

    Redirect correctly after file upload.

--- datafiles/urls.py
+++ datafiles/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'upload', views.UploadFile.as_view(), name="upload-file"),
-    url(r'upload-complete/(?P<file_id>[0-9]+)/$', views.upload_complete, name="upload-complete"),
+    url(r'upload/$', views.UploadFile.as_view(), name="upload-file"),
+    url(r'file/(?P<file_id>[0-9]+)/$', views.view_file, name="view-file"),
 ]
--- datafiles/views.py
+++ datafiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 
 from utils.views import BaseView
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 from .forms import FileUploadForm
+
+from models import DataFile
 
 class UploadFile(BaseView):
     template = 'datafiles/upload_file.django.html'
@@ -26,15 +28,17 @@ class UploadFile(BaseView):
             datafile = form.save(commit=False)
             datafile.uploaded_by = request.user            
             datafile.save()
-            return redirect("upload-complete", file_id=datafile.id)
+            return redirect("view-file", file_id=datafile.id)
         return render(request, self.template, self.get_context(
             title=self.title,
             form=form,
         ))
         
             
-def upload_complete(request, file_id):
-    return render(request, 'datafiles/upload_complete.django.html', {
-        'title': 'Upload Complete!',
+def view_file(request, file_id):
+    file = get_object_or_404(DataFile, pk=file_id)
+    return render(request, 'datafiles/view_file.django.html', {
+        'title': file.name,
+        'description': file.description,
         'year': datetime.now().year,
     })
