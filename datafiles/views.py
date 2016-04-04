6b0fa618923c0ddaa4fb6e363a325b928259b415
Will Dicharry <wdicharry@tnc.org>
2016-04-04 10:03:08 -0600

    Add download button on view page for data files.

--- datafiles/views.py
+++ datafiles/views.py
@@ -14,31 +14,32 @@ from models import DataFile
 class UploadFile(BaseView):
     template = 'datafiles/upload_file.django.html'
     title = 'Upload File'
-    
+
     def get(self, request):
         form = FileUploadForm()
         return render(request, self.template, self.get_context(
             title = self.title,
             form = form,
         ))
-        
+
     def post(self, request):
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             datafile = form.save(commit=False)
-            datafile.uploaded_by = request.user            
+            datafile.uploaded_by = request.user
             datafile.save()
             return redirect("view-file", file_id=datafile.id)
         return render(request, self.template, self.get_context(
             title=self.title,
             form=form,
         ))
-        
-            
+
+
 def view_file(request, file_id):
     file = get_object_or_404(DataFile, pk=file_id)
     return render(request, 'datafiles/view_file.django.html', {
         'title': file.name,
         'description': file.description,
+        'url': file.data_file.url,
         'year': datetime.now().year,
     })
--- utils/views.py
+++ utils/views.py
@@ -5,11 +5,11 @@ from django.views.generic import View
 from datetime import datetime
 
 class BaseView(View):
-    """A base class that populates common info that all views need."""
+    """A base class that populates common information that all views need."""
     context = {
         'year': datetime.now().year,
     }
-    
+
     def get_context(self, **kwargs):
         context = self.context
         for name, value in kwargs.iteritems():
