cfb77d1a45a44eea2ad447f4f4fcf5107b28553e
Will Dicharry <wdicharry@tnc.org>
2016-08-05 10:23:55 -0600

    Reorganize scenario edit page.

--- scenarios/forms.py
+++ scenarios/forms.py
@@ -14,6 +14,7 @@ class ScenarioForm(forms.ModelForm):
             "source_type",
             "critical_season_begin",
             "critical_season_end",
+            "crop_mix",
 
             # Gage fields
             "gage_location",
